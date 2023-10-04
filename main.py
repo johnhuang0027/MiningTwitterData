1aadaa7ba029443c1b93ed8f213b5e74b2809cb3
johnhuang0027 <112910208+johnhuang0027@users.noreply.github.com>
2023-10-03 22:43:59 -0700

    add access to personal data
    will print status, tweets, and followers

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tweepy
 from tweepy import OAuthHandler
 
+def process_or_store(tweet):
+    print(json.dumps(tweet))
+
+
 consumer_key = 'F0N5SEUKJngMz2AT0xUQKxbl0'
 consumer_secret = '413SDix7h1EjRvzSrCBWDVQA6kcRAIITh75ulSV0ZiN7Ulp1a6'
 access_token = '1709258612210888705-2R7nGhqxzIdY6q723i9qRbBkvvmiSd'
@@ -11,3 +15,19 @@ auth.set_access_token(access_token,access_secret)
 
 api= tweepy.API(auth)
 
+#read my own timeline with limit of 10 tweets
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    print(status.text)
+
+#these do the same thing but with json
+for status in tweepy.Cursor(api.friends).items(10):
+    process_or_store(status._json)
+   
+#prints all the followers 
+for friend in tweepy.Cursor(api.friends).items():
+    process_or_store(friend._json)
+
+# prints all the tweets
+for tweet in tweepy.Cursor(api.user_timeline).items():
+    process_or_store(tweet._json)
+    
